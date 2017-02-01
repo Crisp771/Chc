467b6ac4f934681be13712436bb84c0504bd38eb
Cris <Cris@CRIS-LTP>
2017-02-01 10:10:47 +0000

    Add project files.

---
+++ Chc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chc.MvcApplication" Language="C#" %>
---
+++ Chc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chc.Startup))]
+namespace Chc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
