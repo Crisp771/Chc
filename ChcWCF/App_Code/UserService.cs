d4687322d9f3349d23ce0a7b7c6c629df1a69f98
Cris <cwilson@eventmasters.co.uk>
2017-02-03 16:44:51 +0000

    WCF now in correct folder

---
+++ ChcWCF/App_Code/UserService.cs
@@ -0,0 +1,22 @@
+﻿using ChcObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserService" in code, svc and config file together.
+public class UserService : IUserService
+{
+    public IList<User> GetUserList()
+    {
+        return new User().GetUsers().ToList();
+    }
+
+    public User ValidateUserByUsernameAndPassword(LoginViewModel user)
+    {
+        var _user = new User(user);
+        return _user.ValidateUserByUsernameAndPassword(_user);
+    }
+}
