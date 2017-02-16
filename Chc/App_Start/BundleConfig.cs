67a026275cc1854273f339d476def1e96e1b7064
Cris <cwilson@eventmasters.co.uk>
2017-02-16 11:14:29 +0000

    Dashboard

--- Chc/App_Start/BundleConfig.cs
+++ Chc/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Chc
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Styles.css"));
         }
     }
 }
